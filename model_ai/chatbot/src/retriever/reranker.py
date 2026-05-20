68c11558619e3054b4ff8db4035ded5e751e7a3a
vngyyn <tranvnguyen0910@gmail.com>
2026-05-20 13:16:28 +0700

    feat(retriever): change reranker from hardcode to use model BAAI/bge-reranker-v2-m3

--- model_ai/chatbot/src/retriever/reranker.py
+++ model_ai/chatbot/src/retriever/reranker.py
@@ -1,37 +1,40 @@
+from sentence_transformers import CrossEncoder
+
 class Reranker:
     def __init__(self):
-        pass
+        self.model = CrossEncoder(
+            "BAAI/bge-reranker-v2-m3",
+            device="cuda"
+        )
 
     def rerank(self, query, docs, top_k=5):
-        query_lower = query.lower()
-        
+
+        pairs = []
+
         for doc in docs:
             data = doc.get("data", {})
-            address = str(data.get("address", "")).lower()
-            name = str(data.get("location_name", "")).lower()
-            category = str(data.get("category", "")).lower()
-            
-            score = doc.get("score", 0)
 
-            # [CẢI TIẾN] Dynamic Matching thay vì Hardcode
-            # Lấy từng từ khóa trong tên địa điểm để check với query
-            name_words = [word for word in name.split() if len(word) > 2] 
-            matched_words = sum(1 for word in name_words if word in query_lower)
-            
-            if matched_words > 0:
-                score += (0.1 * matched_words) # Boost nhẹ cho mỗi từ trùng
-                
-            # Trùng nguyên cụm tên
-            if name in query_lower:
-                score += 0.4 # Boost rất mạnh
+            text = f"""
+            Tên địa điểm: {data.get('location_name', '')}
 
-            # Liên quan du lịch
-            if any(k in query_lower for k in ["du lịch", "tham quan", "địa điểm", "chơi", "ăn"]):
-                if any(k in category for k in ["du lịch", "tham quan", "phiêu lưu", "khám phá"]):
-                    score += 0.1
+            Địa chỉ: {data.get('address', '')}
 
-            doc["rerank_score"] = score
+            Loại hình: {data.get('category', '')}
 
-        # Sort lại theo rerank_score
-        docs = sorted(docs, key=lambda x: x["rerank_score"], reverse=True)
+            Mô tả: {data.get('description', '')}
+            """
+
+            pairs.append((query, text))
+
+        scores = self.model.predict(pairs)
+
+        for doc, score in zip(docs, scores):
+            doc["rerank_score"] = float(score)
+
+        docs = sorted(
+            docs,
+            key=lambda x: x["rerank_score"],
+            reverse=True
+        )
+
         return docs[:top_k]
